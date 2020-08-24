165d2a273d01ff8247a6d89b15a3f15007040b19
Denis Shvayko <denis.shvayko@phystech.edu>
2020-08-24 19:58:00 +0300

    A6.15: first try create Game

---
+++ js/game.js
@@ -0,0 +1,63 @@
+const numDivs = 36;
+const maxHits = 10;
+
+let hits = 0;
+let bedHits = -1;
+let firstHitTime = 0;
+function round() {
+  // FIXME: надо бы убрать "target" прежде чем искать новый (сделал)
+
+  let divSelector = randomDivId();
+  $(divSelector).addClass("target");
+  // TODO: помечать target текущим номером (сделал)
+  $(divSelector).text(hits+1);
+  // FIXME: тут надо определять при первом клике firstHitTime (сделал)
+  if (hits === 1){
+    firstHitTime = getTimestamp();
+  }else if(hits === maxHits) {
+    endGame();
+  }
+}
+
+function endGame() {
+  // FIXME: спрятать игровое поле сначала (сделал)
+  $(".grid-wrapper").hide();
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $("#total-time-played").text(totalPlayedSeconds);
+  $("#total-score").text(maxHits+bedHits+1);
+  $("#win-message").removeClass("d-none");
+}
+
+function handleClick(event) {
+  // FIXME: убирать текст со старых таргетов. Кажется есть .text? (сделал)
+
+  if ($(event.target).hasClass("target")) {
+    $(event.target).removeClass("target");
+    $(event.target).text("");
+    hits = hits + 1;
+    round();
+  }
+  else if($(event.target).hasClass("grid-item")){
+      $(event.target).addClass("miss");
+      $(event.target).text(bedHits);
+      bedHits = bedHits - 1;
+  }
+  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss (сделал)
+}
+
+function init() {
+  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
+  $("#button-start").click(function() {
+    $(".cont1").removeClass("d-none");
+    $(".cont2").addClass("d-none");
+  });
+
+  round();
+  $(".grid-item").click(handleClick);
+  $("#button-reload").click(function() {
+    location.reload();
+  });
+}
+
+$(document).ready(init);
